8a8a2916e9e6a0e3304f558a4d687c34c4784d2f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2022-10-06 10:15:23 +0200

    polyscope

--- cmake/dgtal.cmake
+++ cmake/dgtal.cmake
@@ -12,6 +12,7 @@ FetchContent_Declare(
     DGtal
     GIT_REPOSITORY https://github.com/DGtal-team/DGtal.git
     GIT_SHALLOW    TRUE
+    GIT_TAG eff424a02a0a5c84327a48009cb5f23e91d288e1
     )
 FetchContent_MakeAvailable(DGtal)
 
---
+++ examplePolyscope.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <DGtal/base/Common.h>
+#include <DGtal/helpers/StdDefs.h>
+#include <DGtal/helpers/Shortcuts.h>
+#include <DGtal/helpers/ShortcutsGeometry.h>
+
+#include <polyscope/polyscope.h>
+#include <polyscope/surface_mesh.h>
+
+
+using namespace DGtal;
+using namespace Z3i;
+
+// Using standard 3D digital space.
+typedef Shortcuts<Z3i::KSpace>         SH3;
+typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
+
+int main()
+{
+  polyscope::init();
+  
+  auto params = SH3::defaultParameters() | SHG3::defaultParameters() |  SHG3::parametersGeometryEstimation();
+  
+  auto h=0.5; //gridstep
+  params( "polynomial", "goursat" )( "gridstep", h );
+  auto implicit_shape  = SH3::makeImplicitShape3D  ( params );
+  auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
+  auto K               = SH3::getKSpace( params );
+  auto binary_image    = SH3::makeBinaryImage( digitized_shape, params );
+  auto surface         = SH3::makeDigitalSurface( binary_image, K, params );
+  auto embedder        = SH3::getCellEmbedder( K );
+  SH3::Cell2Index c2i;
+  auto surfels         = SH3::getSurfelRange( surface, params );
+  auto primalSurface   = SH3::makePrimalPolygonalSurface(c2i, surface);
+  
+  //Need to convert the faces
+  std::vector<std::vector<std::size_t>> faces;
+  for(auto &face: primalSurface->allFaces())
+    faces.push_back(primalSurface->verticesAroundFace( face ));
+    
+  auto digsurf = polyscope::registerSurfaceMesh("Primal surface", primalSurface->positions(), faces);
+  digsurf->setEdgeWidth(1.0);
+  digsurf->setEdgeColor({1.,1.,1.});
+  
+  polyscope::show();
+  return 0;
+}
