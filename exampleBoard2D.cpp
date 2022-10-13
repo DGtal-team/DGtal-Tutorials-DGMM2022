ec69d3829cbb61ad03a851afaacd7c1604cc79fb
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2022-10-13 11:16:10 +0200

    abord with DSS

--- exampleBoard2D.cpp
+++ exampleBoard2D.cpp
@@ -2,20 +2,26 @@
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
 #include <DGtal/io/boards/Board2D.h>
+#include <DGtal/geometry/curves/ArithmeticalDSS.h>
 
 using namespace DGtal;
 
 int main()
 {
   Z2i::Point a(0,0);
-  Z2i::Point b(10,10);
-  Z2i::Point c(4,7);
+  Z2i::Point b(33,43);
+
   Z2i::Domain domain(a,b);
-  
+
+  NaiveDSS8<Z2i::Integer> dss(a,b);
+
   Board2D board;
   board << domain;
-  board << a << b << c;
+  board << dss;
   
   board.saveSVG("simple-domain.svg");
   return 0;
 }
+
+
+
